7335e0ac64ad03d78c7346dd7e75587e73a598a8
Parmida <Mehrnikoo.net@gmail.com>
2025-05-28 17:24:02 +0330

    some changes has been made

---
+++ app/view/Items/list.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo htmlspecialchars($pageTitle); ?></title>
+    </head>
+<body>
+    <h1><?php echo htmlspecialchars($pageTitle); ?></h1>
+
+    <a href="/online_shop/items/new">Add New Item</a>
+    <hr>
+
+    <?php if (empty($items)): ?>
+        <p>No items in stock yet!</p>
+    <?php else: ?>
+        <ul>
+            <?php foreach ($items as $item): ?>
+                <li>
+                    <strong><?php echo htmlspecialchars($item['name']); ?></strong>
+                    (€<?php echo htmlspecialchars($item['price']); ?>) -
+                    <a href="/online_shop/items/show/<?php echo $item['id']; ?>">Details</a> |
+                    <a href="/online_shop/items/edit/<?php echo $item['id']; ?>">Edit</a> |
+                    <a href="/online_shop/items/delete/<?php echo $item['id']; ?>">Delete</a> </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+</body>
+</html>
