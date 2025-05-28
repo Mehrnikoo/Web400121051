fd632922bc5d199034e66600d33eb2986654e7bf
Parmida <Mehrnikoo.net@gmail.com>
2025-05-28 23:12:43 +0330

    404 Not Found

--- app/views/404.php
+++ app/views/404.php
@@ -1,3 +1,17 @@
-<?php
-echo ("404 not found");
-?>
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 - Page Not Found</title>
+    <style>
+        body { font-family: sans-serif; text-align: center; padding-top: 50px; }
+        h1 { font-size: 4em; }
+        p { font-size: 1.2em; }
+    </style>
+</head>
+<body>
+    <h1>404</h1>
+    <p>Oops! The page you were looking for could not be found.</p>
+    <p><a href="/web400121051/">Go back to the Homepage</a></p>
+</body>
+</html>
